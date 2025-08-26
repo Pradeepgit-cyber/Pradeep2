beedb634580b69f898bbf02672c727d18c7868fa
Pradeep Baraik <pradeepbaraik@pradeepbaraikcleargit config --global user.name Pradeep>
2025-08-26 20:29:07 +0530

    New selenium Changes

---
+++ PythonSeleium/dynamicdropdown.py
@@ -0,0 +1,30 @@
+import time
+
+from  selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+driver.maximize_window()
+driver.find_element(By.ID, 'autosuggest').send_keys('ind')    #click and type ind
+time.sleep(2)
+countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")  #will show all the list of webelements
+print(len(countries))      #will count the number of countries in the list
+
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+time.sleep(5)
--- PythonSeleium/locators.py
+++ PythonSeleium/locators.py
@@ -1,7 +1,11 @@
 import time
 
+
+#from select import select
+
 from  selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 driver.get('https://rahulshettyacademy.com/angularpractice/')
@@ -12,16 +16,29 @@ driver.find_element(By.NAME, 'email').click()
 driver.find_element(By.NAME, 'email').send_keys('hello@gmail.com')
 driver.find_element(By.ID, 'exampleInputPassword1' ).send_keys('123456')
 driver.find_element(By.ID, 'exampleCheck1').click()
-driver.find_element(By.XPATH, "//input[@type='submit']").click()
 #Custom xpath - //tagname[@attribute='value']
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys('Pradeep')
 # Custom css -   tagname[attribute='value']
 driver.find_element(By.CSS_SELECTOR, '#inlineRadio1').click()   #ID
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys('HelloPradeep')
+
+#static dropdown
+dropdown = Select(driver.find_element(By.ID, 'exampleFormControlSelect1'))
+dropdown.select_by_index(0)
+time.sleep(2)
+dropdown.select_by_visible_text('Female')
+
+
+
+
+
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+
 message = driver.find_element(By.CLASS_NAME, 'alert-success').text
 print(message)
 
 assert "Success" in message
-driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys('HelloPradeep')
+
 
 
 
